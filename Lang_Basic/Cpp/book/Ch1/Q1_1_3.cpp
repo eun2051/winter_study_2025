ca091a569f0a986c63d5009795f1e6b2c467daaf
eun2051 <genie5518@yu.ac.kr>
2025-12-27 16:15:33 +0900

    cpp

---
+++ Lang_Basic/Cpp/book/Ch1/Q1_1_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    
+    for (int j = 1; j <= 9; j++) {
+        cout << n << "*" << j << "=" << n * j << endl;
+    }
+}
