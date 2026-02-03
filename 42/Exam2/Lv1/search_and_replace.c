e911e434647646199ebc8d607b73fb0d8b302b1e
eun2051 <genie5518@naver.com>
2026-02-03 13:21:57 +0900

    lv1

--- 42/Exam2/Lv1/search_and_replace.c
+++ 42/Exam2/Lv1/search_and_replace.c
@@ -4,3 +4,21 @@ another one (3rd argument).
 If the number of arguments is not 3, just display a newline.
 If the second argument is not contained in the first one (the string)
 then the program simply rewrites the string followed by a newline. */
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    if (ac != 4) {
+        write(1, "\n", 1);
+        return 0;
+    }
+    int i = 0, j;
+    while (av[1][i] != '\0' && av[2][1] == '\0' && av[3][1] == '\0') {
+        if (av[1][i] == av[2][0])
+            av[1][i] = av[3][0];
+        write(1, &av[1][i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+    return 0;
+}
--- 42/Exam2/Lv1/ulstr.c
+++ 42/Exam2/Lv1/ulstr.c
@@ -2,3 +2,32 @@
 Other characters remain unchanged.
 You must display the result followed by a '\n'.
 If the number of arguments is not 1, the program displays '\n'. */
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        write(1, "\n", 1);
+        return 0;
+    }
+    int i = 0;
+    char a;
+    while (av[1][i] != '\0') {
+        if (av[1][i] >= 'a' && av[1][i] <= 'z') {
+            a = av[1][i] - 32;
+            write(1, &a, 1);
+            i++;
+            continue;
+        }
+        if (av[1][i] >= 'A' && av[1][i] <= 'Z') {
+            a = av[1][i] + 32;
+            write(1, &a, 1);
+            i++;
+            continue;
+        }
+        write(1, &av[1][i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+    return 0;
+}
