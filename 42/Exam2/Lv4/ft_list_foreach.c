18ab7dbd187367be0081996b22f978ed1105ace1
eun2051 <genie5518@naver.com>
2026-02-06 18:02:53 +0900

    lv4

--- 42/Exam2/Lv4/ft_list_foreach.c
+++ 42/Exam2/Lv4/ft_list_foreach.c
@@ -13,5 +13,11 @@ The function pointed to by f will be used as follows:
 
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
+	t_list	*ptr = begin_list;
 
+	while (ptr)
+	{
+		(*f)(ptr->data);
+		ptr = ptr->next;
+	}
 }
--- 42/Exam2/Lv4/ft_list_remove_if.c
+++ 42/Exam2/Lv4/ft_list_remove_if.c
@@ -6,7 +6,29 @@ It will be declared as follows :
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
 
 cmp takes two void* and returns 0 when both parameters are equal. */
+#include "ft_list.h"
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
+	t_list	*curr = *begin_list;
+	t_list	*prev = NULL;
+	t_list	*remove = NULL;
+
+	if (begin_list == NULL || *begin_list == NULL)
+		return ;
+	while (curr) {
+		if ((*cmp)(curr->data, data_ref) == 0) {
+			remove = curr;
+			if (prev == NULL)
+				*begin_list = curr->next;
+			else
+				prev->next = curr->next;
+			curr = curr->next;
+			free(remove);
+		}
+		else {
+			prev = curr;
+			curr = curr->next;
+		}
+	}
 }
